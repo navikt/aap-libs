2ecaefe113d4315196cd9332b8f4dd200523435b
Robin <robin.tordly@nav.no>
2022-04-21 12:52:09 +0200

    use implementations

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,9 +1,10 @@
 dependencies {
-    api("ch.qos.logback:logback-classic:1.2.11")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
-    api("org.apache.kafka:kafka-clients:3.1.0")
-    api("org.apache.kafka:kafka-streams:3.1.0")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
+    implementation("org.apache.kafka:kafka-clients:3.1.0")
+    implementation("org.apache.kafka:kafka-streams:3.1.0")
+
     implementation("io.confluent:kafka-streams-avro-serde:7.0.1") {
         exclude("org.apache.kafka", "kafka-clients")
     }
