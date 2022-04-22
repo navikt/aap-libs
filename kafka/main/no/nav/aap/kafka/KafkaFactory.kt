804b713f4d24afdd5d5fa95f0e25573d3f7d1d66
Robin <robin.tordly@nav.no>
2022-04-22 14:39:05 +0200

    pass config to functions

--- kafka/main/no/nav/aap/kafka/KafkaFactory.kt
+++ kafka/main/no/nav/aap/kafka/KafkaFactory.kt
@@ -9,19 +9,19 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.Producer
 
 interface KafkaFacade {
-    fun <V : Any> createConsumer(topic: Topic<V>): Consumer<String, V>
-    fun <V : Any> createProducer(topic: Topic<V>): Producer<String, V>
+    fun <V : Any> createConsumer(config: KafkaConfig, topic: Topic<V>): Consumer<String, V>
+    fun <V : Any> createProducer(config: KafkaConfig, topic: Topic<V>): Producer<String, V>
 }
 
-class KafkaFactory(private val config: KafkaConfig) : KafkaFacade {
-    override fun <V : Any> createConsumer(topic: Topic<V>): Consumer<String, V> = KafkaConsumer(
+object KafkaFactory : KafkaFacade {
+    override fun <V : Any> createConsumer(config: KafkaConfig, topic: Topic<V>): Consumer<String, V> = KafkaConsumer(
         config.consumer + mapOf(
             CommonClientConfigs.CLIENT_ID_CONFIG to "client-${topic.name}",
             ConsumerConfig.GROUP_ID_CONFIG to "${topic.name}-1",
         ), topic.keySerde.deserializer(), topic.valueSerde.deserializer()
     )
 
-    override fun <V : Any> createProducer(topic: Topic<V>): Producer<String, V> = KafkaProducer(
+    override fun <V : Any> createProducer(config: KafkaConfig, topic: Topic<V>): Producer<String, V> = KafkaProducer(
         config.producer + mapOf(CommonClientConfigs.CLIENT_ID_CONFIG to "client-${topic.name}"),
         topic.keySerde.serializer(),
         topic.valueSerde.serializer()
--- kafka/test/no/nav/aap/kafka/KafkaFactoryTest.kt
+++ kafka/test/no/nav/aap/kafka/KafkaFactoryTest.kt
@@ -2,7 +2,6 @@ package no.nav.aap.kafka
 
 import no.nav.aap.kafka.serde.json.JsonSerde
 import no.nav.aap.kafka.streams.Topic
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -10,10 +9,15 @@ class KafkaFactoryTest {
 
     @Test
     fun consumer() {
-        val factory = KafkaFactory(defaultKafkaTestConfig.copy(credstorePsw = ""))
-        val consumer = factory.createConsumer(Topic("topic", JsonSerde.jackson()))
+        val config = defaultKafkaTestConfig.copy(credstorePsw = "")
+        val consumer = KafkaFactory.createConsumer(config, Topic("topic", JsonSerde.jackson()))
         val groupId = consumer.groupMetadata().groupId()
         assertEquals("topic-1", groupId)
     }
 
+    @Test
+    fun producer() {
+        val config = defaultKafkaTestConfig.copy(credstorePsw = "")
+        KafkaFactory.createProducer(config, Topic("topic", JsonSerde.jackson()))
+    }
 }
