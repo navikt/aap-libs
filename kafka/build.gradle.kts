ee57d38e5bdf9f0974bb8e61c4db1a44af8f2a09
Robin <robin.tordly@nav.no>
2022-04-25 09:59:07 +0200

    expose rocksdbjni lib

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -3,8 +3,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
     implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
-    implementation("org.rocksdb:rocksdbjni:6.29.4.1") // M1 support
 
+    api("org.rocksdb:rocksdbjni:6.29.4.1") // M1 support
     api("org.apache.kafka:kafka-clients:3.1.0")
     api("org.apache.kafka:kafka-streams:3.1.0") {
         exclude("org.rocksdb", "rocksdbjni")
