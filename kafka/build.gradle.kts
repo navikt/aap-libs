3ad663e4950f6f3f4a4442ff587e3795da0e4727
Robin <robin.tordly@nav.no>
2022-04-25 09:43:06 +0200

    kafka streams support on mac M1

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -3,9 +3,12 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
     implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
+    implementation("org.rocksdb:rocksdbjni:6.29.4.1") // M1 support
 
     api("org.apache.kafka:kafka-clients:3.1.0")
-    api("org.apache.kafka:kafka-streams:3.1.0")
+    api("org.apache.kafka:kafka-streams:3.1.0") {
+        exclude("org.rocksdb", "rocksdbjni")
+    }
     api("io.confluent:kafka-streams-avro-serde:7.0.1") {
         exclude("org.apache.kafka", "kafka-clients")
     }
