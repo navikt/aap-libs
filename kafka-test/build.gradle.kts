88a08d66c0d60f7702c6f847a03be5881e96d283
Robin <robin.tordly@nav.no>
2022-04-26 09:39:41 +0200

    expose kafka libraries as supposed

--- kafka-test/build.gradle.kts
+++ kafka-test/build.gradle.kts
@@ -1,12 +1,13 @@
 dependencies {
     api("org.apache.kafka:kafka-streams-test-utils:3.1.0") {
-        exclude("org.rocksdb", "rocksdbjni") // included in :kafka
+        exclude("org.rocksdb", "rocksdbjni")
     }
 
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
     implementation(project(":kafka"))
+
     implementation(kotlin("test"))
 
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
     implementation("io.confluent:kafka-streams-avro-serde:7.0.1") {
         exclude("org.apache.kafka", "kafka-clients")
     }
--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,24 +1,23 @@
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
-
     api("org.rocksdb:rocksdbjni:6.29.4.1") // M1 support
+    api("org.apache.kafka:kafka-streams:3.1.0") {
+        exclude("org.rocksdb", "rocksdbjni")
+    }
+
     api("org.apache.kafka:kafka-clients:3.1.0") {
         version {
             strictly("3.1.0")
         }
     }
-    api("org.apache.kafka:kafka-streams:3.1.0") {
-        exclude("org.rocksdb", "rocksdbjni")
-    }
-
-    implementation("io.confluent:kafka-streams-avro-serde:7.0.1") {
+    api("io.confluent:kafka-streams-avro-serde:7.0.1") {
         exclude("org.apache.kafka", "kafka-clients")
     }
+
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
 
     testImplementation(kotlin("test"))
 }
