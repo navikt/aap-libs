4ac7dc2e550a112bf318ccafcbe6236ee7c32d16
Robin <robin.tordly@nav.no>
2022-04-26 09:55:19 +0200

    use correct kafka client version

--- kafka-test/build.gradle.kts
+++ kafka-test/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     api("org.apache.kafka:kafka-streams-test-utils:3.1.0") {
         exclude("org.rocksdb", "rocksdbjni")
+        exclude("org.apache.kafka", "kafka-clients")
     }
 
     implementation(project(":kafka"))
--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,14 +1,12 @@
 dependencies {
     api("org.rocksdb:rocksdbjni:6.29.4.1") // M1 support
+    api("org.apache.kafka:kafka-clients:3.1.0")
+
     api("org.apache.kafka:kafka-streams:3.1.0") {
         exclude("org.rocksdb", "rocksdbjni")
+        exclude("org.apache.kafka", "kafka-clients")
     }
 
-    api("org.apache.kafka:kafka-clients:3.1.0") {
-        version {
-            strictly("3.1.0")
-        }
-    }
     api("io.confluent:kafka-streams-avro-serde:7.0.1") {
         exclude("org.apache.kafka", "kafka-clients")
     }
