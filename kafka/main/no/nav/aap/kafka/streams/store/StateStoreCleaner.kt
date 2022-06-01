a897059b1f0be3b71810a9cb5e6be7da8b18b59c
Anders Østby <anders.ostby@nav.no>
2022-06-01 07:51:41 +0200

    Oppdatert StateStoreCleaner til å slette spesifikk key så den ikke trenger å loope gjennom hele state store

--- kafka/main/no/nav/aap/kafka/streams/store/StateStoreCleaner.kt
+++ kafka/main/no/nav/aap/kafka/streams/store/StateStoreCleaner.kt
@@ -1,6 +1,7 @@
 package no.nav.aap.kafka.streams.store
 
 import org.apache.kafka.streams.kstream.KTable
+import org.apache.kafka.streams.kstream.Named
 import org.apache.kafka.streams.processor.PunctuationType
 import org.apache.kafka.streams.processor.api.Processor
 import org.apache.kafka.streams.processor.api.ProcessorContext
@@ -11,22 +12,19 @@ import org.apache.kafka.streams.state.ValueAndTimestamp
 import org.slf4j.LoggerFactory
 import java.time.Duration
 
-internal class StateStoreCleaner<V>(
+private class StateStoreCleaner<K, V>(
     private val storeName: String,
-    private val predicate: (value: ValueAndTimestamp<V>) -> Boolean,
-) : Processor<String, V, Void, Void> {
-    override fun process(record: Record<String, V>) {}
+    private val keysToDelete: MutableList<K>,
+) : Processor<K, V, Void, Void> {
+    override fun process(record: Record<K, V>) {}
 
     override fun init(context: ProcessorContext<Void, Void>) {
-        val store = context.getStateStore<KeyValueStore<String, ValueAndTimestamp<V>>>(storeName)
+        val store = context.getStateStore<KeyValueStore<K, ValueAndTimestamp<V>>>(storeName)
         context.schedule(Duration.ofSeconds(1), PunctuationType.WALL_CLOCK_TIME) {
-            store.all().use { iterator ->
-                iterator.forEach { record ->
-                    if (predicate(record.value)) {
-                        store.delete(record.key)
-                        secureLog.info("Deleted [$storeName] [${record.key}] [${record.value}]")
-                    }
-                }
+            keysToDelete.removeIf { key ->
+                val value = store.delete(key)
+                secureLog.info("Deleted [$storeName] [$key] [$value]")
+                true
             }
         }
     }
@@ -36,10 +34,17 @@ internal class StateStoreCleaner<V>(
     }
 }
 
-fun <V> KTable<String, V>.scheduleCleanup(
+fun <K, V> KTable<K, V>.scheduleCleanup(
     store: String,
-    predicate: (value: ValueAndTimestamp<V>) -> Boolean
+    keysToDelete: MutableList<K>,
+) = scheduleCleanup(store, keysToDelete, "Cleanup $store")
+
+fun <K, V> KTable<K, V>.scheduleCleanup(
+    store: String,
+    keysToDelete: MutableList<K>,
+    name: String,
 ) = toStream().process(
-    ProcessorSupplier { StateStoreCleaner(store, predicate) },
+    ProcessorSupplier { StateStoreCleaner(store, keysToDelete) },
+    Named.`as`(name),
     store
 )
