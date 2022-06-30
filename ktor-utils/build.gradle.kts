a47948e2332bce1fabde6fe7eff56b9ba903f019
Richard Borge <richard.borge@nav.no>
2022-06-30 10:22:52 +0200

    Bumper ktor versjon.
    Co-authored-by: Anders Østby <anders.ostby@nav.no>

--- ktor-utils/build.gradle.kts
+++ ktor-utils/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.0.2")
+    implementation("io.ktor:ktor-server-core:2.0.3")
     implementation("com.sksamuel.hoplite:hoplite-yaml:2.1.5")
 
     testImplementation(kotlin("test"))
