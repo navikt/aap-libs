0e308aea64e1058b6652428d6f5f29a270faf35f
Anders Østby <anders.ostby@nav.no>
2022-07-26 08:29:52 +0200

    Bumper avhengigheter Kotlin til 1.7.10 Gradle til 7.5

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,13 +1,13 @@
 dependencies {
     api("org.apache.kafka:kafka-streams:3.2.0")
-    api("io.confluent:kafka-streams-avro-serde:7.1.1")
+    api("io.confluent:kafka-streams-avro-serde:7.2.1")
 
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.3")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 
     testImplementation(kotlin("test"))
     testImplementation("org.apache.kafka:kafka-streams-test-utils:3.2.0") {
