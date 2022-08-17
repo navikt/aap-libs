aadf4ae354c64d80bd09777607830d86c1e5c9f2
Robin <robin.tordly@nav.no>
2022-08-17 09:02:30 +0200

    oppgraderer hoplite

--- ktor-utils/build.gradle.kts
+++ ktor-utils/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation("io.ktor:ktor-server-core:2.1.0")
-    implementation("com.sksamuel.hoplite:hoplite-yaml:2.5.0")
+    implementation("com.sksamuel.hoplite:hoplite-yaml:2.5.2")
 
     testImplementation(kotlin("test"))
 }
