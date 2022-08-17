ece89bc49a3d5c81e69dc7f531049325a9569bbe
Robin <robin.tordly@nav.no>
2022-08-17 09:02:08 +0200

    oppgraderer ktor

--- ktor-client-auth/build.gradle.kts
+++ ktor-client-auth/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
-    api("io.ktor:ktor-client-content-negotiation:2.0.3")
-    api("io.ktor:ktor-serialization-jackson:2.0.3")
-    api("io.ktor:ktor-client-cio:2.0.3")
-    api("io.ktor:ktor-client-auth:2.0.3")
+    api("io.ktor:ktor-client-content-negotiation:2.1.0")
+    api("io.ktor:ktor-serialization-jackson:2.1.0")
+    api("io.ktor:ktor-client-cio:2.1.0")
+    api("io.ktor:ktor-client-auth:2.1.0")
 
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
 }
