66ad256df3bfdab2d38558b76b0134010187e1cd
Anders Østby <anders.ostby@nav.no>
2022-09-29 12:48:18 +0200

    Legger til muligheten for å kunne consumere med timestampextractor
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>
    Co-authored-by: Richard Borge <richard.borge@nav.no>

--- kafka/main/no/nav/aap/kafka/streams/extension/StreamsBuilderExtension.kt
+++ kafka/main/no/nav/aap/kafka/streams/extension/StreamsBuilderExtension.kt
@@ -5,12 +5,24 @@ import no.nav.aap.kafka.streams.Topic
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.GlobalKTable
 import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.processor.TimestampExtractor
 
 /**
  * @param logValue Logs record values to secure-logs when true
  */
-fun <V> StreamsBuilder.consume(topic: Topic<V>, logValue: Boolean = false): KStream<String, V?> = this
+fun <V> StreamsBuilder.consume(
+    topic: Topic<V>,
+    logValue: Boolean = false
+): KStream<String, V?> = this
     .stream(topic.name, topic.consumed("consume-${topic.name}"))
+    .logConsumed(topic, logValue)
+
+fun <V> StreamsBuilder.consume(
+    topic: Topic<V>,
+    timeStrampExtractor: TimestampExtractor,
+    logValue: Boolean = false
+): KStream<String, V?> = this
+    .stream(topic.name, topic.consumed("consume-${topic.name}").withTimestampExtractor(timeStrampExtractor))
     .logConsumed(topic, logValue)
 
 fun <V> StreamsBuilder.globalTable(table: Table<V>): GlobalKTable<String, V> =
