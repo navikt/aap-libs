97871f21f7bf64fe68af4e77f41ddc3e1174dae2
Anders Østby <anders.ostby@nav.no>
2022-10-03 09:08:58 +0200

    Og bumper logback...

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api("org.apache.kafka:kafka-streams:3.3.0")
 
-    implementation("ch.qos.logback:logback-classic:1.4.1")
+    implementation("ch.qos.logback:logback-classic:1.4.3")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
