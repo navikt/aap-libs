f5d294659639de047fd656b5500c980f277d65db
Robin <robin.tordly@nav.no>
2022-10-25 10:21:50 +0200

    fix deps

--- ktor-auth-maskinporten/build.gradle.kts
+++ ktor-auth-maskinporten/build.gradle.kts
@@ -3,14 +3,16 @@ plugins {
 }
 
 val ktorVersion = "2.1.2"
-dependencies {
-    api("io.ktor:ktor-client-core:$ktorVersion")
-    api("com.nimbusds:nimbus-jose-jwt:9.25.6")
-    api("io.ktor:ktor-client-cio:$ktorVersion")
-    api("io.ktor:ktor-client-content-negotiation:$ktorVersion")
-    api("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    api("io.ktor:ktor-client-auth:$ktorVersion")
 
+dependencies {
+    implementation("io.ktor:ktor-client-auth:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+
+    implementation("com.nimbusds:nimbus-jose-jwt:9.25.6")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
 
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
