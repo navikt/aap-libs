4d7ce4c7b3f046fb322b90aa3a7feeb3cb9b8a92
Anders Østby <anders.ostby@nav.no>
2022-11-21 08:45:19 +0100

    Bumper avhengigheter

--- kafka-avroserde/build.gradle.kts
+++ kafka-avroserde/build.gradle.kts
@@ -8,12 +8,12 @@ dependencies {
     api("org.apache.kafka:kafka-streams:3.3.1")
     api("io.confluent:kafka-streams-avro-serde:7.3.0")
 
-    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
     implementation("net.logstash.logback:logstash-logback-encoder:7.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.0")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.0")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.10.0")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 
     testImplementation(kotlin("test"))
