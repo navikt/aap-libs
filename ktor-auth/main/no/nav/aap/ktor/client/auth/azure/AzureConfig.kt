9731618ce7a86a2571c76a457bbec8bf61dd0bc3
Richard Borge <richard.borge@nav.no>
2024-02-09 12:50:16 +0100

    Legger til issuer på azure for å få tilgang til denne på auth

--- ktor-auth/main/no/nav/aap/ktor/client/auth/azure/AzureConfig.kt
+++ ktor-auth/main/no/nav/aap/ktor/client/auth/azure/AzureConfig.kt
@@ -6,5 +6,6 @@ data class AzureConfig(
     val tokenEndpoint: String = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
     val clientId: String = getEnvVar("AZURE_APP_CLIENT_ID"),
     val clientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET"),
-    val jwksUri: String = getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI")
+    val jwksUri: String = getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI"),
+    val issuer: String = getEnvVar("AZURE_OPENID_CONFIG_ISSUER")
 )
