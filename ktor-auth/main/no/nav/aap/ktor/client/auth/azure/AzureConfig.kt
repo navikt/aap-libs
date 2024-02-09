8320dee7d93558737404e99fb08e19a94b764f52
Richard Borge <richard.borge@nav.no>
2024-02-09 12:43:44 +0100

    Legger til jwksuri på azure for å få tilgang til denne på auth

--- ktor-auth/main/no/nav/aap/ktor/client/auth/azure/AzureConfig.kt
+++ ktor-auth/main/no/nav/aap/ktor/client/auth/azure/AzureConfig.kt
@@ -5,5 +5,6 @@ import no.nav.aap.ktor.client.auth.util.getEnvVar
 data class AzureConfig(
     val tokenEndpoint: String = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
     val clientId: String = getEnvVar("AZURE_APP_CLIENT_ID"),
-    val clientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET")
+    val clientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET"),
+    val jwksUri: String = getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI")
 )
