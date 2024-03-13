0537ed8af76bfb991d2520c4183bc3fd94a7e0ed
Robin <robin.tordly@nav.no>
2024-03-13 21:45:17 +0100

    bump jackson

--- ktor-auth/build.gradle.kts
+++ ktor-auth/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
 
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.16.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.0")
     implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
 
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.0")
